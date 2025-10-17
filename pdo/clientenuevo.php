b1f7cfa6275afe3305553da928664d63d7f56483
david <davsermel@gmail.com>
2025-10-17 12:04:09 +0200

    pasarimfo

--- pdo/clientenuevo.php
+++ pdo/clientenuevo.php
@@ -1,3 +1,37 @@
 <?php 
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include "funciones.php";
+$dni = $_GET["dni"];
+$nombre = $_GET["nombre"];
+$direccion = $_GET["direccion"];
+$localidad = $_GET["localidad"];
+$provincia = $_GET["provincia"];
+$telefono = $_GET["telefono"];
+$email = $_GET["email"];
+
+$conexion = obtenerconexion();
+$stmt = $conexion->prepare(
+ 'insert into Cliente (dni,nombre,direccion,localidad,provincia,telefono,email)
+  values (:dni, :nombre, :direccion, :localidad, :provincia, :telefono , :email)'   
+);
+
+$rows = $stmt->execute([
+    ':dni' => $dni,
+    ':nombre' => $nombre,
+    ':direccion' => $direccion,
+    ':localidad' => $localidad,
+    ':provincia' => $provincia,
+    ':telefono' => $telefono,
+    ':email' => $email
+]);
+
+if($rows == 1)
+echo "a";
+
+
+
+
+
+}
 ?>
--- pdo/index.php
+++ pdo/index.php
@@ -3,4 +3,6 @@ include "clienteclass.php";
 
 $cliente = new Cliente();
 $cliente->muestra();
+
+echo "<a href=clientenuevo.html>Crear cliente</a>"
 ?>
