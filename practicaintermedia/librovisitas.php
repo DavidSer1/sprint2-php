6f3da12facc0807ef2d6b9d5f09557c8e743c056
david <davsermel@gmail.com>
2025-11-10 12:57:49 +0100

    imfo

--- practicaintermedia/librovisitas.php
+++ practicaintermedia/librovisitas.php
@@ -1,7 +1,17 @@
 <?php
 
+if(isset($_GET["mensaje"])){
+    $mensaje = $_GET["mensaje"];
+    echo $mensaje . "<br>";
+}
+leerarchivo();
+
 function leerarchivo(){
-    
+    $archivo = fopen("visitas.txt", "r");
+    $tamano = filesize("visitas.txt");
+$texto = fread($archivo, $tamano);
+echo $texto . "<br>";
+fclose($archivo);
 }
 
 
