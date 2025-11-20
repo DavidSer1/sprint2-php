58e3819ebebb01ea43121c0edc3ef5349310750b
David <davsermel@gmail.com>
2025-11-20 10:45:21 +0100

    creacion iniciada

--- clientenuevo.php
+++ clientenuevo.php
@@ -8,10 +8,64 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
      $provincia = $_POST["provincia"];
      $telefono = $_POST["telefono"];
      $email = $_POST["dni"];
+
+     if(insertarcliente($dni,$nombre,$direccion,$localidad,$provincia,$telefono,$email)){
+        $mensaje = "Cliente creado correctamente";
+        header("Location: index.php?mensaje=$mensaje");
+     }
+     else{
+      $mensaje = "El cliente no se ha creado";
+        header("Location: index.php?mensaje=$mensaje");
+     }
 }
 
 
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creacion del cliente</title>
+</head>
+<body>
+    <div> 
+<label for="dni">Dni</label>
+<input type="text" name="dni" id="dni">
+</div>
+
+    <div> 
+<label for="nombre">Nombre</label>
+<input type="text" name="nombre" id="nombre">
+</div>
+
+    <div> 
+<label for="direccion">direccion</label>
+<input type="text" name="direccion"  id="direccion">
+</div>
+
+    <div> 
+<label for="localidad">localidad</label>
+<input type="text" name="localidad"  id="localidad">
+</div>
+
+    <div> 
+<label for="provincia">provincia</label>
+<input type="text" name="provincia"  id="provincia">
+</div>
+
+    <div> 
+<label for="telefono">Telefono</label>
+<input type="text" name="telefono"  id="telefono">
+</div>
+
+    <div> 
+<label for="email">Email</label>
+<input type="text" name="email"  id="email">
+</div>
+
+
+
+</body>
+</html>
--- funciones.php
+++ funciones.php
@@ -1,7 +1,20 @@
 <?php 
 include "Conexion.class.php";
+
 function insertarcliente($dni,$nombre,$direccion,$localidad,$provincia,$telefono,$email){
 $conexion = Conexion::obtenerconexion();
+$consulta = $conexion->prepare("insert into clientes (dni,nombre,direccion,
+localidad,provincia,telefono,email)
+ values (:dni, :nombre , :direccion , :localidad, :provincia, :telefono, :email)");
+ $rows = $consulta->execute([':dni'=> $dni, 
+ ':nombre'=> $nombre, ':direccion'=> $direccion, 
+ ':localidad'=> $localidad, ':provincia'=> $provincia, ':telefono' => $telefono, ':email'=> $email]);
+
+ if($rows == 1){
+    return true;
+ } else{
+    return false;
+ }
 
 }
 
